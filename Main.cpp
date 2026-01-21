66487b6e735ed697d094e1fade51143b855eec7b
Base21rizz <shoumikislamofficial@gmail.com>
2026-01-21 20:59:37 +0600

    Circle manipulation

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,34 @@
 #include "raylib.h"
 int main()
 {
-    InitWindow(350, 200, "Axe");
+    // Window dimensions
+    int width{350};
+    int height{200};
+    InitWindow(width, height, "Axe");
+
+    // Circle Coordinates
+    int circle_x{175};
+    int circle_y{100};
+
+    SetTargetFPS(60);
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
+
+        // Game Logic Start
+
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+        if (IsKeyDown(KEY_D))
+        {
+            circle_x += 10;
+        }
+        if (IsKeyDown(KEY_A))
+        {
+            circle_x -= 10;
+        }
+
+        // Game Logic End
         EndDrawing();
     }
 }
